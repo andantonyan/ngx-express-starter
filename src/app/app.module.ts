73249ae16890c2d0383297d2ac8b3060871023af
andantonyan <andantonyan@gmail.com>
2017-08-16 08:22:24 +0400

    core(*): universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    // BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'ng-starter' })
   ],
   providers: [],
   bootstrap: [AppComponent]
