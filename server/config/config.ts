8ee1a36b1f1af9aeba23d812a4613fd6c11541b6
andantonyan <andantonyan@gmail.com>
2017-08-25 12:27:21 +0400

    core(server): update config

--- server/config/config.ts
+++ server/config/config.ts
@@ -4,7 +4,7 @@ import { IConfig } from '../types';
 
 const config: IConfig = {
   port: 3000,
-  secret: 'fRmRO5mqrW6gir6TRM06+FjCmo6frE86oxZ8wL+OvqA=',
+  secret: 'WlXhIaH11Tk3Dg3HV7Sl5Ax33UxYvVoQurcfasq6SH8=',
   fileLog: join(__dirname, '../../../', 'ngx-express-starter.log'),
 };
 
